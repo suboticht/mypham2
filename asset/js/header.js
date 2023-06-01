19b4f1fed725a2d8f9035e2c77dbb0e419b0bc30
thang_hd <thang-hd@if-vietnam.com>
2023-06-01 15:27:00 +0700

    bk

--- asset/js/header.js
+++ asset/js/header.js
@@ -47,7 +47,7 @@ header = `
                         </ul>
                     </li>
                     <li class="main__menu--item">
-                        <a href="./content-product/index.html" class="main__menu--link">GÓC LÀM ĐẸP</a>
+                        <a href="./news/index.html" class="main__menu--link">GÓC LÀM ĐẸP</a>
                     </li>
                 </ul>
             </div>
