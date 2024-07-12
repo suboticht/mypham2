320b7758ba333e885867f8e72a9df6908dfd22b5
suboticht <86337073+suboticht@users.noreply.github.com>
2024-07-12 14:58:47 +0700

    Add files via upload

--- weddingweb/assets/js/common.js
+++ weddingweb/assets/js/common.js
@@ -57,3 +57,96 @@ const main = () => {
 }
 
 main();
+
+// Get the modal
+var modal = document.getElementsByClassName("myModal")[0];
+var modal2 = document.getElementsByClassName("myModal")[1];
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("openModalBtn")[0];
+var btn2 = document.getElementsByClassName("openModalBtn")[1];
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+    document.body.classList.add("no-scroll");
+}
+btn2.onclick = function() {
+    modal2.style.display = "block";
+    document.body.classList.add("no-scroll");
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  document.body.classList.remove("no-scroll");
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+  document.body.classList.remove("no-scroll");
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal || event.target == modal2) {
+    modal.style.display = "none";
+    modal2.style.display = "none";
+    document.body.classList.remove("no-scroll");
+  }
+}
+
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top < $(window).height()
+    );
+}
+
+// fade in
+$('.FadeInDelay').each(function () {
+    if (isInViewport(this)) {
+        $(this).addClass('FadeIn')
+    }
+})
+$(window).on('scroll', function () {
+    $('.FadeInDelay').each(function () {
+        if (isInViewport(this)) {
+            $(this).addClass('FadeIn')
+        }
+    })
+})
+const audio = new Audio('/assets/sounds/my-love.mp3');
+audio.loop = true;
+$(".music").on("click", function() {
+    if (audio.paused) {
+        audio.play();
+        $(".option_list_item").addClass('active');
+    } else {
+        audio.pause();
+        $(".option_list_item").removeClass('active');
+    }
+})
+
+//scroll to top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var goTopBtn = document.getElementById("goTopBtn");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        goTopBtn.style.display = "block";
+    } else {
+        goTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+document.getElementById("goTopBtn").onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
