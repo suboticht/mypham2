bc0f86d0b53446459b2cf8635d4682d21936b087
suboticht <86337073+suboticht@users.noreply.github.com>
2024-07-19 08:20:37 +0700

    Updated JS file via API

--- weddingweb/assets/js/data.js
+++ weddingweb/assets/js/data.js
@@ -1,1 +1,1 @@
-const data = [{"name":"thắng","mess":"Xin chúc mừng"},{"name":"Thịnh","mess":"Chúc mừng hai bạn"}]
+const data = [{"name":"thắng","mess":"Xin chúc mừng"},{"name":"Thịnh","mess":"sdfhsdfhsdh"}]
